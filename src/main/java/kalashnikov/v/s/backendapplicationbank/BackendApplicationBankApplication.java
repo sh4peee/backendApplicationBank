47e574966592c2f675dc9fbdeaa2a444495ab012
sh4peee <vadim.kalashnikov.9595@mail.ru>
2024-05-24 14:30:28 +0300

    Begin

---
+++ src/main/java/kalashnikov/v/s/backendapplicationbank/BackendApplicationBankApplication.java
@@ -0,0 +1,13 @@
+package kalashnikov.v.s.backendapplicationbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplicationBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplicationBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kalashnikov/v/s/backendapplicationbank/BackendApplicationBankApplicationTests.java
@@ -0,0 +1,13 @@
+package kalashnikov.v.s.backendapplicationbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
